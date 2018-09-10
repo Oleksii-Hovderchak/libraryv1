b0cb8cbd20d07aa758469387971b1bb7c0b9cd18
Oleksii Hovderchak <Oleksii_Hovderchak@epam.com>
2018-09-10 19:09:29 +0300

    Structure of web application

---
+++ library-api/src/main/java/ua/nure/library/LibraryV1Application.java
@@ -0,0 +1,18 @@
+package ua.nure.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for starting the application
+ */
+@SpringBootApplication
+public class LibraryV1Application {
+    /**
+     * Runs the application
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryV1Application.class, args);
+    }
+}
